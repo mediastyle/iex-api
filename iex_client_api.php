fa340a6a973a76ff4583f7918963df054e94edb1
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2011-12-20 12:16:17 +0100

    added IEX_URL definition

--- iex_client_api.php
+++ iex_client_api.php
@@ -5,6 +5,7 @@
  * @copyright Copyright (C) 2011-2012, MediaStyle (http://mediastyle.dk)
  * @package IEXApi
  */
+define('IEX_API','http://localhost/iphoenix/index.php');
 
 class IexClientApi {
   private $ch = null;
@@ -18,17 +19,21 @@ class IexClientApi {
   public function open($key){
     $ch = curl_init();
     curl_setopt($ch,CURLOPT_URL,IEX_URL);
+    curl_setopt($ch,CURLOPT_HEADER,FALSE);
     curl_setopt($ch,CURLOPT_POST,TRUE);
 
     $this->ch = $ch;
   }
 
-  public function query($type,$action,$data=array()){
+  public function query($type,$action,$data=array(),$config=array()){
     $post = $this->post;
     $post['type'] = $type;
     $post['action'] = $action;
+    foreach($config as $key=>$value){
+      $post[$key] = $value;
+    }
     $post['data'] = $data;
-    curl_setopt(CURLOPT_POSTFIELDS,http_build_url($post));
+    curl_setopt($this->ch,CURLOPT_POSTFIELDS,http_build_url($post));
     return curl_exec($this->ch);
   }
 
