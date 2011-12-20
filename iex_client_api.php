12870de73d1897cc245addf053c1e8110379608a
Ronni Elken Lindsgaard <ronni.lindsgaard@gmail.com>
2011-12-20 14:05:01 +0100

    Made the client working

--- iex_client_api.php
+++ iex_client_api.php
@@ -5,19 +5,21 @@
  * @copyright Copyright (C) 2011-2012, MediaStyle (http://mediastyle.dk)
  * @package IEXApi
  */
-define('IEX_API','http://localhost/iphoenix/index.php');
+define('IEX_URL','http://localhost/iphoenix/index.php');
 
 class IexClientApi {
   private $ch = null;
   private $post = array();
 
   public function __construct($key,$secret){
-    $this->open($key);
+    $this->open();
     $this->post['secret'] = $secret;
+    $this->post['key'] = $key;
   }
 
-  public function open($key){
+  public function open(){
     $ch = curl_init();
+
     curl_setopt($ch,CURLOPT_URL,IEX_URL);
     curl_setopt($ch,CURLOPT_HEADER,FALSE);
     curl_setopt($ch,CURLOPT_POST,TRUE);
@@ -33,8 +35,21 @@ class IexClientApi {
       $post[$key] = $value;
     }
     $post['data'] = $data;
-    curl_setopt($this->ch,CURLOPT_POSTFIELDS,http_build_url($post));
+    curl_setopt($this->ch,CURLOPT_POSTFIELDS,$this->build_post($post));
     return curl_exec($this->ch);
+  }
+
+  private function build_post($fields=array(),$prefix =
+  '',$postfix=''){
+    $values = array();
+    foreach($fields as $field=>$value){
+      if(is_array($value)){
+        $values[] = $this->build_post($value,$prefix . $field . $postfix .'[',']');
+      } else {
+        $values[] = $prefix . $field . $postfix . '=' . $value;
+      }
+    }
+    return implode($values,'&');
   }
 
   public function close(){
